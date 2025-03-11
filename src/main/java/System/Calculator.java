ac1fe221bd97d44f401e7259adb2952d85fc1b2c
LENOVO <aaqibrlwn@gmail.com>
2025-03-12 00:13:34 +0530

    Encapsulated the user input

--- src/main/java/System/Calculator.java
+++ src/main/java/System/Calculator.java
@@ -1,4 +1,22 @@
 package System;
 
 public class Calculator {
+
+    private float num1, num2;
+
+    public float getNum1() {
+        return num1;
+    }
+
+    public float getNum2() {
+        return num2;
+    }
+
+    public void setNum1(float num1) {
+        this.num1 = num1;
+    }
+
+    public void setNum2(float num2) {
+        this.num2 = num2;
+    }
 }
